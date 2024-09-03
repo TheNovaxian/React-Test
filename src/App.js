5449c321d93d0b29de122429a55146317ad0c860
TheNovaxian <shaquille-36@hotmail.com>
2024-09-03 13:50:54 -0400

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React meow
+          Learn React meow mix
         </a>
       </header>
     </div>
